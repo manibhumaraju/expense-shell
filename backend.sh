975b61e9e0473b341d6e44c204263a557c12302c
manibhumaraju <manishvarmab@gmail.com>
2024-08-30 16:44:32 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -61,7 +61,31 @@ VALIDATE $? "Downloading backend application code"
 
 cd /app
 rm -rf /app/* #remove the existing code
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
-# cd /app
-# npm install
+
+npm install &>>$LOG_FILE
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+#load the data before running the baground
+ 
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h mysql.manibhumaraju.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Deamon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+ 
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarting backend"
+
+
+
+
+
